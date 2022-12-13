a12c4961b5a178cb45761fc3297335084953486e
jiwagon <jiwagon11@gmail.com>
2022-12-12 20:38:27 -0500

    refined acc page

--- app/src/main/java/com/example/tripper_2/Guide.java
+++ app/src/main/java/com/example/tripper_2/Guide.java
@@ -1,10 +1,5 @@
 package com.example.tripper_2;
 
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-
-
 public class Guide {
 
     String text;
